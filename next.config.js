f36712283d506b437d86bfc9d91ad66a0bdc4602
Geoff <geoffereth@gmail.com>
2022-09-22 15:29:25 -0700

    Removes old hn-hired implementation

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/latest' : '',
-  webpack: (config) => {
-    config.output.publicPath = `/latest${config.publicPath}`;
-    return config;
-  }
-}
